97ada790b6c0b98f216bec67c6ca9a3e01e42f63
brady <brady.m.adams@gmail.com>
2019-05-17 09:39:37 -0600

    setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+import setuptools
+
+version = '19.0.0'
+
+build_num = os.getenv('BUILD_NUMBER')
+
+if build_num:
+    version += '.' + str(build_num)
+
+setuptools.setup(
+    name='threemf',
+    version=version,
+    author='Teton Simulation',
+    author_email='info@tetonsim.com',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'numpy-stl']
+)
